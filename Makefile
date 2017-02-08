671f6e4f798871820dcd9e919b81a5c9f9d31cd4
rreboot <r_reboot@mail.ru>
2017-02-08 14:17:04 +0500

    first step

--- Makefile
+++ Makefile
@@ -2,10 +2,10 @@ install:
 	npm install
 
 start:
-	npm run babel-node -- src/bin/gendiff.js
+	npm run babel-node -- src/bin/gendiff.js --help
 
 publish:
 	npm publish
 
 make lint:
-	npm run eslint 
+	npm run eslint
--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,5 +1,11 @@
 #!/usr/bin/env node
 
-import data from '../data';
+import commander from 'commander';
 
-data();
+const program = commander;
+program
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'Output format')
+  .arguments('<first> <second>')
+  .parse(process.argv);
